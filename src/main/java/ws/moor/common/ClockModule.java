8e10e73e1380a5702cabf3bd1b55e06df2dd37ee
Patrick Moor <patrick@moor.ws>
2014-09-09 21:55:28 -0700

    update maven dependencies to latest versions update to JDK8 getting rid of Joda time in favor of java.time.*

--- src/main/java/ws/moor/common/ClockModule.java
+++ src/main/java/ws/moor/common/ClockModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Patrick Moor <patrick@moor.ws>
+ * Copyright 2014 Patrick Moor <patrick@moor.ws>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,11 @@
 package ws.moor.common;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-import org.joda.time.Instant;
 
-public interface Clock {
-  Instant now();
+import java.time.Clock;
 
-  static class SystemClock implements Clock {
-    @Inject private SystemClock() {}
-    @Override public Instant now() {
-      return new Instant(System.currentTimeMillis());
-    }
-  }
-
-  static class Module extends AbstractModule {
-    @Override protected void configure() {
-      bind(Clock.class).to(SystemClock.class).in(Singleton.class);
-    }
+public class ClockModule extends AbstractModule {
+  @Override protected void configure() {
+    bind(Clock.class).toInstance(Clock.systemUTC());
   }
 }
